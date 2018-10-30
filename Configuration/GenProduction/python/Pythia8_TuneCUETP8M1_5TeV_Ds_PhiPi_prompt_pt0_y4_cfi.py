9ff5ff12686ad60d2406755e11aed1249e745d83
Wei <liwei810812@gmail.com>
2018-10-30 05:16:59 +0100

    New configurations

---
+++ Configuration/GenProduction/python/Pythia8_TuneCUETP8M1_5TeV_Ds_PhiPi_prompt_pt0_y4_cfi.py
@@ -0,0 +1,78 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(5020.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010.DEC'),
+            operates_on_particles = cms.vint32(),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
+            list_forced_decays = cms.vstring('MyD_s+','MyD_s-'),
+      user_decay_embedded= cms.vstring(
+"""
+Alias        MyD_s+                 D_s+
+Alias        MyD_s-                 D_s-
+ChargeConj   MyD_s-                 MyD_s+
+Alias        Myphi                  phi
+Decay MyD_s+
+    1.000           Myphi     pi+     SVS;
+Enddecay
+CDecay MyD_s-
+Decay Myphi
+    1.000           K+        K-      VSS;
+Enddecay
+End
+"""
+       )
+
+
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(
+            'HardQCD:all = on',
+            'PhaseSpace:pTHatMin = 0', #min pthat
+        ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+        )
+    )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+partonfilter = cms.EDFilter("PythiaFilter",
+    ParticleID = cms.untracked.int32(4) # 4 for prompt D0 and 5 for non-prompt D0
+    )
+
+DsDaufilter = cms.EDFilter("PythiaMomDauFilter",
+    ParticleID = cms.untracked.int32(431),
+    MomMinPt = cms.untracked.double(0),
+    MomMinEta = cms.untracked.double(-10.0),
+    MomMaxEta = cms.untracked.double(10.0),
+    DaughterIDs = cms.untracked.vint32(333, 211),
+    NumberDaughters = cms.untracked.int32(2),
+    DaughterID = cms.untracked.int32(333),
+    DescendantsIDs = cms.untracked.vint32(321 , -321),
+    NumberDescendants = cms.untracked.int32(2),
+)
+Dsrapidityfilter = cms.EDFilter("PythiaFilter",
+      ParticleID = cms.untracked.int32(431),
+                  MinPt = cms.untracked.double(0),
+                  MaxPt = cms.untracked.double(1000.),
+                  MinRapidity = cms.untracked.double(-4.0),
+                  MaxRapidity = cms.untracked.double(4.0),
+                  )
+
+ProductionFilterSequence = cms.Sequence(generator*partonfilter*DsDaufilter*Dsrapidityfilter)
