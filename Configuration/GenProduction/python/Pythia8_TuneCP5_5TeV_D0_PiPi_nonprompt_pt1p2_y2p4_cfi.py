02c413977b3f501960f4a8ba593ff05642253fd1
Yousen Zhang <zysright@gmail.com>
2020-06-23 07:48:31 +0200

    modified configs for D0toKK and D0toPiPi, added an exmaple md. All are under test

--- Configuration/GenProduction/python/Pythia8_TuneCP5_5TeV_D0_PiPi_nonprompt_pt1p2_y2p4_cfi.py
+++ Configuration/GenProduction/python/Pythia8_TuneCP5_5TeV_D0_PiPi_nonprompt_pt1p2_y2p4_cfi.py
@@ -13,7 +13,22 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
             decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2010.DEC'),
             operates_on_particles = cms.vint32(),
             particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
-            user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/D0_Kpi.dec'),
+            #user_decay_file = cms.vstring('GeneratorInterface/ExternalDecays/data/D0_Kpi.dec'),
+            user_decay_embedded = cms.vstring(
+# https://github.com/cms-data/GeneratorInterface-EvtGenInterface/blob/master/D0_Kpi.dec
+"""
+Alias myD0      D0
+Alias myanti-D0 anti-D0
+ChargeConj myanti-D0 myD0
+#
+Decay myD0
+1.000   pi- pi+  PHSP;
+Enddecay
+CDecay myanti-D0
+
+End
+"""
+            ),
             list_forced_decays = cms.vstring('myD0', 'myanti-D0')
         ),
         parameterSets = cms.vstring('EvtGen130')
@@ -37,14 +52,14 @@ generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
 
 partonfilter = cms.EDFilter("PythiaFilter",
     ParticleID = cms.untracked.int32(5) # 4 for c and 5 for b quark
-	)
+  )
 
 D0Daufilter = cms.EDFilter("PythiaMomDauFilter",
     ParticleID = cms.untracked.int32(421),
     MomMinPt = cms.untracked.double(0.0),
     MomMinEta = cms.untracked.double(-10.),
     MomMaxEta = cms.untracked.double(10.),
-    DaughterIDs = cms.untracked.vint32(211, -321),
+    DaughterIDs = cms.untracked.vint32(211, -211),
     NumberDaughters = cms.untracked.int32(2),
     NumberDescendants = cms.untracked.int32(0),
     BetaBoost = cms.untracked.double(0.0),
---
+++ Configuration/GenProduction/scripts/cmsdriver_commands_for_D0toPiPi_PbPb5p02TeV_2018.md
@@ -0,0 +1,9 @@
+# I removed "--nThreads 2".
+# GEN-SIM
+cmsDriver.py Configuration/GenProduction/python/Pythia8_TuneCP5_5TeV_D0_PiPi_prompt_pt1p2_y2p4_cfi.py \
+--fileout file:Pythia8_TuneCP5_5TeV_D0_PiPi_prompt_pt1p2_y2p4.root \
+--pileup_input "dbs:/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbAutumn18GS-103X_upgrade2018_realistic_HI_v11-v1/GEN-SIM" \
+--mc --eventcontent RAWSIM --pileup HiMixGEN --datatier GEN-SIM --conditions 103X_upgrade2018_realistic_HI_v11 \
+--beamspot MatchHI --step GEN,SIM \ 
+--scenario HeavyIons --geometry DB:Extended --era Run2_2018_pp_on_AA \
+--python_filename Pythia8_TuneCP5_5TeV_D0_PiPi_prompt_pt1p2_y2p4_cfg.py --no_exec -n 1000
